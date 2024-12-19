1106fbaf4ab498b12b45cb0c72b4db6daa5c0aca
Enrico Meister <profiler5000@gmail.com>
2024-12-19 02:41:38 +0100

    integriert unity

---
+++ Test_Unity_C-_Dez2024_V2/Assets/Scripts/Scenemanager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Profiler.Unity_Test_Project2024
+{
+    public class Scenemanager : U
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
